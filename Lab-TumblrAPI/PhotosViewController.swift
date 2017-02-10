8b463d44e78c28b076e5e45c725c0c121a5df32a
Deep S Randhawa <deepsaranr@gmail.com>
2017-02-09 19:04:11 -0500

    Added UIRefreshControl

--- Lab-TumblrAPI/PhotosViewController.swift
+++ Lab-TumblrAPI/PhotosViewController.swift
@@ -47,12 +47,47 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         return cell
     }
 
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task : URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        //print("responseDictionary: \(responseDictionary)")
+                        
+                        // Recall there are two fields in the response dictionary, 'meta' and 'response'.
+                        // This is how we get the 'response' field
+                        let responseFieldDictionary = responseDictionary["response"] as! NSDictionary
+                        
+                        // This is where you will store the returned array of posts in your posts property
+                        self.posts = responseFieldDictionary["posts"] as! [NSDictionary]
+                        self.mainTableView.reloadData()
+                        refreshControl.endRefreshing()
+                    }
+                }
+        });
+        task.resume()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mainTableView.delegate = self
         mainTableView.dataSource = self
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        self.mainTableView.insertSubview(refreshControl, at: 0)
 
         // Do any additional setup after loading the view.
         let url = URL(string:"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")
@@ -83,7 +118,7 @@ class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDa
         });
         task.resume()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
